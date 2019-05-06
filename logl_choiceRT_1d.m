f19def84d831973bc4af662dc8dd78f9144c8a84
Ariel Zylberberg <arielzylberberg@users.noreply.github.com>
2019-05-06 13:20:12 -0400

    speeded up the convolution with non-dec distrib

--- logl_choiceRT_1d.m
+++ logl_choiceRT_1d.m
@@ -1,7 +1,4 @@
-function [nlogl,pPred] = logl_choiceRT_1d(P,choice,rt,coh,ndt_m,ndt_s)
-% Computes the negative log-likelihood of the parameters given the choice
-% and reaction time data. 
-% Comments to: ariel.zylberberg@gmail.com
+function [logl,pPred] = logl_choiceRT_1d(P,choice,rt,coh,ndt_m,ndt_s)
 
 t = P.t;
 
@@ -14,7 +11,23 @@ nt = length(t);
 dt = t(2)-t(1);
 ntr = length(P.drift);
 
-ndt = normpdf(t,ndt_m,ndt_s)*dt;
+
+method = 2;
+switch method
+    case 1
+        ndt = normpdf(t,ndt_m,ndt_s)*dt;
+    case 2
+        % Oct 2018
+        pd = makedist('Normal','mu',ndt_m,'sigma',ndt_s);
+        pd_trunc = truncate(pd,0,inf);
+        ndt = pd_trunc.pdf(P.t)*dt;
+end
+
+% should speed things up
+imax = find(cumsum(ndt)>0.999999,1);
+ndt = ndt(1:imax);
+
+
 upRT = conv2(1,ndt(:),P.up.pdf_t);
 loRT = conv2(1,ndt(:),P.lo.pdf_t);
 upRT = upRT(:,1:nt);
@@ -33,8 +46,16 @@ for i=1:ncoh
     p_lo(inds) = loRT(i,J);
 end
 
+%correct prob by the prob. that the ndt
+%is below zero. ?
+% ptrunk = 1./(1-normcdf(0,rt-ndt_m,ndt_s));
+% p_up = p_up.*ptrunk;
+% p_lo = p_lo.*ptrunk;
+
 %clip
 p_up(p_up<eps) = eps;
 p_lo(p_lo<eps) = eps;
 pPred = p_up.*(choice==1) + p_lo.*(choice==0);
-nlogl = -nansum(log(pPred));
+
+logl = -sum(log(pPred));
+% logl = -nanmean(log(pPred));
